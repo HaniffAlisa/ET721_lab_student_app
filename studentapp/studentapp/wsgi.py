c561fcb206070ac6faf77ff8e3f4870862a3bab7
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-12-03 18:47:14 +0000

    updated wsgi

--- studentapp/studentapp/wsgi.py
+++ studentapp/studentapp/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'studentapp.settings'
+
+# Activate your virtual environment
+activate_this = '/home/yourusername/yourprojectdirectory/myenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 
