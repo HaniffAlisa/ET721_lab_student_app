7edaf7f25521d65a4cc5d54595093b90f07dea29
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-12-06 18:23:46 +0000

    all 3 apps running

--- studentapp/studentapp/wsgi.py
+++ studentapp/studentapp/wsgi.py
@@ -17,12 +17,12 @@ if project_home not in sys.path:
 
 # Set the Django settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'studentapp.settings'
-
+"""
 # Activate your virtual environment
 activate_this = '/home/yourusername/yourprojectdirectory/myenv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
-
+"""
 # Import Django's WSGI handler
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
